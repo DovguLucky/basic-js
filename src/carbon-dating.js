af6141c0da88d98864867a8790a57c27ab0f410b
DovguLucky <dovgluch98@gmail.com>
2023-04-18 00:12:01 +0300

    add:new ex

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,25 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(c14) {
+  let res = 0;
+
+  if (typeof c14 === 'string') {
+  let sampleActivity = Number(c14)
+  console.log(sampleActivity)
+  let HALF_LIFE_PERIOD = 5.73 * (10 ** 3);
+  let MODERN_ACTIVITY = 0.693;
+  let res = (Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / Math.log(2);
+      // res = Math.floor((0.3142 * (15.63 / sampleActivity)) / (MODERN_ACTIVITY / HALF_LIFE_PERIOD))
+  }else{
+    res=false;
+  }
+  return res
 }
 
 module.exports = {
-   dateSample
-//    (c14) {
-//     let res = 0;
-
-//     if (typeof c14 === 'string') {
-//     let sampleActivity = Number(c14)
-//     console.log(sampleActivity)
-//     let HALF_LIFE_PERIOD = 5.73 * (10 ** 3);
-//     let MODERN_ACTIVITY = 0.693;
-//         res = Math.floor((0.3142 * (15.63 / sampleActivity)) / (MODERN_ACTIVITY / HALF_LIFE_PERIOD))
-//     }else{
-//       res=false;
-//     }
-//     return res
-// }
+   dateSample   
 };
